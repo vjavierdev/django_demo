b05c34cd0b1ebe0ce6d04437884dd35b7e0dd93d
unknown <killuanhenriquez@gmail.com>
2023-08-30 16:52:03 -0400

    subiendo proyecto v1.0

---
+++ myapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def hello(request, username):
+    print(username)
+    return HttpResponse("<h1>Hello World  "+ username+"</h1>")
+
+def about(request):
+    return render(request, 'about.html')
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, id):
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse('Tasks: %s' % task.title)
+
+
